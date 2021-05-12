30fb8661220c6194da3a23b9356164ea036ba20e
Robin Khiv <robinkhiv@gmail.com>
2021-05-12 13:34:15 -0700

    added associative table

--- SQL/DDL/example.sql
+++ SQL/DDL/example.sql
@@ -16,15 +16,27 @@ CREATE TABLE product (
 CREATE TABLE orders (
     id INT NOT NULL AUTO_INCREMENT,
     fk_customer_id INT NOT NULL,
-    fk_product_id INT NOT NULL,
+    -- fk_product_id INT NOT NULL,
     PRIMARY KEY (id),
     KEY (fk_customer_id),
     KEY (fk_product_id),
     FOREIGN KEY (fk_customer_id) 
-        REFERENCES customer(id),
-    FOREIGN KEY (fk_product_id) 
-        REFERENCES product(id)
+        REFERENCES customer(id)
+    -- FOREIGN KEY (fk_product_id) 
+    --     REFERENCES product(id)
 );
 
-ALTER TABLE orders 
+
+CREATE TABLE product-order (
+    fk_order_id INT NOT NULL,
+    PRIMARY KEY (fk_order_id, fk_product_id),
+    FOREIGN KEY (fk_order_id)
+        REFERENCES orders(id),
+    FOREIGN KEY (fk_product_id)
+        REFERENCES product(id)
+)
+
+ALTER TABLE product-order 
 ADD quantity INT DEFAULT 1;
+
+
