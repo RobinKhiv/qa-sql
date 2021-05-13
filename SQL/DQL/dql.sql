74613c25701093a47b877782a7843c43154b7fb4
Robin Khiv <robinkhiv@gmail.com>
2021-05-13 08:19:25 -0700

    rk:added bonus DQL exercises

--- SQL/DQL/dql.sql
+++ SQL/DQL/dql.sql
@@ -26,3 +26,10 @@ SELECT title, special_features FROM film WHERE special_features = "Deleted Scene
 
 -- 9) Find the titles of all the horror movies.
 SELECT f.title, c.name FROM film_category fc JOIN category c ON fc.category_id = c.category_id JOIN film f ON f.film_id = fc.film_id WHERE fc.category_id = 11;
+
+-- BONUS
+-- 1) List the name of every language in reverse-alphabetical order. (HINT: use 'ORDER BY' and 'SELECT DISTINCT')
+-- 2) List the full names of every actor whose surname ends with '-son' in alphabetical order by their forename (HINT: use %son%)
+-- 3) Using HAVING COUNT, reverse-alphabetically list the last names that are not repeated.
+select last_name, Count(last_name) from actor group by last_name HAVING COUNT(last_name) = 1 order by last_name desc;
+-- 4) Which actor has appeared in the most films? (HINT: use joins, COUNT() and GROUP BY)
