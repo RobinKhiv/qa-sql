ac294b68761680ea384bf59e7a5b682293c85b04
Robin Khiv <robinkhiv@gmail.com>
2021-05-13 09:04:31 -0700

    rk:added comments and completed database entry

--- SQL/DQL/school.sql
+++ SQL/DQL/school.sql
@@ -1,5 +1,8 @@
+-- School database exercise
 CREATE DATABASE school;
 use school;
+
+-- 1) Create a database for a school. It should include at least three tables - students, classes, enrolments
 
 DROP TABLE IF EXISTS enrollment;
 DROP TABLE IF EXISTS class;
@@ -31,3 +34,32 @@ CREATE TABLE enrollment (
     FOREIGN KEY (fk_class_id) 
         REFERENCES class(class_id)
 );
+
+-- 2) You should insert at least 3 students and classes into your tables.
+INSERT INTO student (first_name, last_name, age)
+VALUES 
+    ('Robbie', 'Smith', 22),
+    ('Rob', 'Doe', 23),
+    ('Robi', 'Rob', 24);
+
+
+INSERT INTO class (subject, description, level)
+VALUES 
+    ('Database Design', 'Learn all about making a ERD model', 1),
+    ('Database Manipulation Language', 'Learn to manipulate', 2),
+    ('Database Query Language', 'Learn how to query', 1);
+
+-- 3) Each student should be enrolled in at least 2 classes.
+INSERT INTO enrollment (fk_student_id, fk_class_id)
+VALUES
+    (1,2),
+    (1,1),
+    (2,1),
+    (2,3),
+    (3,2),
+    (3,1);
+
+-- 4) Use joins to find out which classes they should attend.
+SELECT s.first_name, s.last_name, c.subject  from enrollment e 
+    JOIN class c ON c.class_id = e.fk_class_id  
+    JOIN student s ON s.student_id = e.fk_student_id; 
